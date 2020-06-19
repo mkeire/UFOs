96334238f0d165c36325c5c71df38b697dea565c
Max Keire <maxkeire@tutanota.com>
2020-06-18 20:54:51 -0700

    Corrected buildTable after filtering scope.

--- static/js/app.js
+++ static/js/app.js
@@ -23,16 +23,25 @@ data.forEach((dataRow) => {
 
 // Filter and present data when button is clicked
 function handleClick() {
+    // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
+    
+     // Check to see if a date was entered and filter the
+    // data using that date.
     if (date) {
-            filteredData = filteredData.filter(row => row.datetime === date);
-            buildTable(filteredData);
-    };
-};
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+}
 
-// Listening for button click
+// Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
 
-// Build table when page loads
+// Build the table when the page loads
 buildTable(tableData);
